a96f97b54788b0ece6426dd2644f785e1cb85d57
Ilya-Rebikov <ilya.rebikov.2000@mail.ru>
2022-12-19 10:35:19 +0300

    Small fix.

--- src/CourseWork.DistributionAPI/Services/HttpClientsFactory.cs
+++ src/CourseWork.DistributionAPI/Services/HttpClientsFactory.cs
@@ -36,7 +36,7 @@ namespace CourseWork.DistributionAPI.Services
             var serversCount = int.Parse(_configuration["ServersCount"]);
             for (int i = 0; i < serversCount; i++)
             {
-                var baseUrl = _configuration["BaseUrl"] + ":" + (5002 + i);
+                var baseUrl = _configuration["BaseUrl"] + ":" + (int.Parse(_configuration["BasePort"]) + i);
                 servers.Add(RestClient.For<IComputingHttpClient>(baseUrl));
             }
 
